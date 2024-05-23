4f8fcc30e7d5636b3f5a38b9933dd5d2c5f23ae9
星如雨 <liyun200104@163.com>
2024-05-23 10:49:11 +0800

    🎉 init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nust3 Demo
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true, // 是否开启服务端渲染 `SSR` | `SPA` 
+  devtools: { enabled: true },
+  modules:[
+    '@unocss/nuxt'
+  ]
+})
