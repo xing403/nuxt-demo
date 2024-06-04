54d6ea9259ae7176fdcac77321a21443d17627b5
星如雨 <liyun200104@163.com>
2024-06-04 17:27:08 +0800

    ✨ feat(admin layout): category & tags pages

--- components/admin/AdminSidebar.vue
+++ components/admin/AdminSidebar.vue
@@ -2,15 +2,14 @@
   <el-menu router h-100vh>
     <el-menu-item index="/admin">首页</el-menu-item>
     <el-menu-item index="/admin/post">我的文章</el-menu-item>
-    <el-menu-item index="文章管理">我的分类</el-menu-item>
-    <el-menu-item index="文章管理">我的标签</el-menu-item>
+    <el-menu-item index="/admin/category">我的分类</el-menu-item>
+    <el-menu-item index="/admin/tags">我的标签</el-menu-item>
     <el-menu-item index="/">返回前台</el-menu-item>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
 const route = useRoute()
-console.log(route)
 </script>
 
 <style></style>
---
+++ pages/admin/tags/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ['login'],
+  layout: 'admin'
+})
+</script>
+
+<style>
+
+</style>
